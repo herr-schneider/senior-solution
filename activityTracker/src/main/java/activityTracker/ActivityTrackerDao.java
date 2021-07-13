7efe6b8efa60a53f09ba8f1231b153303b734b32
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-07-13 08:11:21 +0200

    Activity begin

--- activityTracker/src/main/java/activityTracker/ActivityTrackerDao.java
+++ activityTracker/src/main/java/activityTracker/ActivityTrackerDao.java
@@ -1,21 +1,53 @@
 package activityTracker;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class ActivityTrackerDao {
 
-EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu");
-EntityManager entityManager = entityManagerFactory.createEntityManager();
+    @Autowired
+    private ModelMapper modelMapper;
 
-public void createActivity(){
-    entityManager.getTransaction().begin();
-    Activity activity = new Activity();
-    entityManager.persist(activity);
-    entityManager.getTransaction().commit();
-    entityManager.close();
-    entityManagerFactory.close();
-}
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu");
+    private EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    public void createActivity() {
+        entityManager.getTransaction().begin();
+        Activity activity = new Activity();
+        entityManager.persist(activity);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    public ActivityDto findActivityByID(long id) {
+        Activity activity = entityManager.find(Activity.class, id);
+        entityManager.close();
+        entityManagerFactory.close();
+        return modelMapper.map(activity, ActivityDto.class);
+    }
+
+    public void deleteByID(long id) {
+        entityManager.getTransaction().begin();
+        Activity activity = entityManager.find(Activity.class, id);
+        entityManager.remove(activity);
+        entityManager.getTransaction().commit();
+    }
+
+    public List<Activity> findAllActivity(String type) {
+        entityManager.getTransaction().begin();
+        List<Activity> result = entityManager.createQuery("select a from Activity a",
+                Activity.class)
+                .getResultList();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+        return result;
+    }
 }
