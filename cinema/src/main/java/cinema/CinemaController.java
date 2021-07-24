6c51b843d37f0a5b04ed06797669169e993e005a
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-07-24 07:47:13 +0200

    minor fix

--- cinema/src/main/java/cinema/CinemaController.java
+++ cinema/src/main/java/cinema/CinemaController.java
@@ -3,16 +3,14 @@ package cinema;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/api/cinema")
